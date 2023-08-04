d742624cde50b9dc5200555b8d413e58845dc722
Mikael Rekola <mikael.rekola@gmail.com>
2023-08-04 23:26:36 +0300

    add clock.h

---
+++ clock.h
@@ -0,0 +1,37 @@
+#ifndef _CLOCK_H_
+#define _CLOCK_H_
+
+#include <stdint.h>
+
+#ifdef _WIN32
+
+#include <Windows.h>
+
+static inline uint64_t system_time(){
+  LARGE_INTEGER time, freq;
+  if (!QueryPerformanceFrequency(&freq)){
+    return 0;
+  }
+  if (!QueryPerformanceCounter(&time)){
+    return 0;
+  }
+  return 1000000ll * time.QuadPart / freq.QuadPart;
+}
+
+//  Posix/Linux
+#else
+
+#include <sys/time.h>
+#include <time.h>
+
+static inline uint64_t system_time(){
+  struct timeval time;
+  if (gettimeofday(&time,NULL)){
+    //  Handle error
+    return 0;
+  }
+  return 1000000ll * time.tv_sec + time.tv_usec;
+}
+
+#endif
+#endif
