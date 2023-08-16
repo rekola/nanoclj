017cbe8bab972bf7db784450aee4a1ce9577f2db
Mikael Rekola <mikael.rekola@gmail.com>
2023-08-16 15:07:43 +0300

    add c++ bindings

---
+++ cpp/Interpreter.cpp
@@ -0,0 +1,82 @@
+#include "Interpreter.h"
+
+#include <unistd.h>
+
+using namespace nanoclj;
+
+static void out_callback(const char * s, size_t len, void * data) {
+  fprintf(stderr, "out callback %d\n", (int)len);
+  if (len > 0) {
+    auto clojure = reinterpret_cast<Interpreter *>(data);
+    clojure->add_chars(std::string_view(s, len));
+  }
+}
+
+static void error_callback(const char * s, size_t len, void * data) {
+  if (len > 0) {
+    auto clojure = reinterpret_cast<Interpreter *>(data);
+    clojure->add_error_chars(std::string_view(s, len));
+  }
+}
+
+static nanoclj_val_t object_invoke_callback(nanoclj_t * sc, void * object, nanoclj_val_t args) {
+  return args;
+}
+
+Interpreter::Interpreter() {
+  auto sc = nanoclj_init_new();
+  if (!sc) {
+    fprintf(stderr, "initialization failed\n");
+  } else {
+    sc_ = sc;
+
+    nanoclj_set_input_port_file(sc, stdin);
+    nanoclj_set_output_port_callback(sc, &out_callback);
+    nanoclj_set_error_port_callback(sc, &error_callback);
+    nanoclj_set_external_data(sc, this);
+    nanoclj_set_object_invoke_callback(sc, &object_invoke_callback);
+    
+    chdir("assets/nanoclj");
+    auto fin = fopen("init.clj", "rt");
+    if (fin) {
+      nanoclj_load_named_file(sc, fin, "init.clj");
+      fclose(fin);
+    }
+    
+    auto sym = nanoclj_eval_string(sc, "(ns-interns *ns*)");
+    for ( ; sym.as_uint64 != sc->EMPTY.as_uint64; sym = sc->vptr->pair_cdr(sym)) {
+      auto v = sc->vptr->pair_car(sym);
+      if (sc->vptr->is_symbol(v)) {
+	symbols_.emplace_back(sc->vptr->symname(v));
+      } else if (sc->vptr->is_mapentry(v)) {
+	symbols_.emplace_back(sc->vptr->symname(sc->vptr->pair_car(v)));
+      }
+    }
+  }
+}
+
+Interpreter::~Interpreter() {
+  nanoclj_deinit(sc_);
+  free(sc_);
+}
+
+Value *
+Interpreter::eval(const std::string & expression) {
+  nanoclj_eval_string(sc_, expression.c_str());
+  return nullptr;
+}
+
+void
+Interpreter::eval_print(const std::string & expression) {
+  nanoclj_val_t v = nanoclj_eval_string(sc_, expression.c_str());
+  nanoclj_val_t c = sc_->vptr->cons(sc_, sc_->vptr->def_symbol(sc_, "prn"),
+				    sc_->vptr->cons(sc_, v, sc_->EMPTY));
+  nanoclj_eval(sc_, c);
+}
+
+#if 0
+void
+Interpreter::execute(const std::string & expression) {
+  nanoclj_load_string(sc_, expression.c_str());
+}
+#endif
---
+++ cpp/Interpreter.h
@@ -0,0 +1,70 @@
+#ifndef _NANOCLJ_INTERPRETER_H_
+#define _NANOCLJ_INTERPRETER_H_
+
+#include "nanoclj.h"
+#include "nanoclj_priv.h"
+
+#include <vector>
+#include <string>
+#include <string_view>
+#include <functional>
+
+namespace nanoclj {
+  using UserFn = nanoclj_val_t(*)(nanoclj_t *, nanoclj_val_t);
+  
+  class Value {
+  public:
+    Value() { }
+
+    int integerValue() {
+      return 0;
+    }
+  };
+  
+  class Interpreter {
+  public:
+    Interpreter();
+    virtual ~Interpreter();
+
+    Value * eval(const std::string & expression);
+    void eval_print(const std::string & expression);
+
+    virtual void print(std::string_view s) { }
+    virtual void print_error(std::string_view s) { }
+
+    void add_chars(std::string_view s) {
+      out_buffer += s;
+      for (int i = static_cast<int>(out_buffer.size()) - 1; i >= 0; i--) {
+	if (out_buffer[i] == '\n') {
+	  fprintf(stderr, "flushing out\n");
+	  print(std::string_view(out_buffer).substr(0, i + 1));
+	  out_buffer.erase(0, i + 1);
+	}
+      }
+    }
+    void add_error_chars(std::string_view s) {
+      error_buffer += s;
+      for (int i = static_cast<int>(error_buffer.size()) - 1; i >= 0; i--) {
+	if (error_buffer[i] == '\n') {
+	  print_error(std::string_view(error_buffer).substr(0, i + 1));
+	  error_buffer.erase(0, i + 1);
+	}
+      }
+    }
+    
+    void define(std::string symbol, UserFn function) {
+      sc_->vptr->nanoclj_intern(sc_, sc_->global_env,
+				sc_->vptr->mk_symbol(sc_, symbol.c_str()), 
+				sc_->vptr->mk_foreign_func(sc_, function));
+    }
+
+    const std::vector<std::string> & getSymbols() const { return symbols_; }
+    
+  private:
+    nanoclj_t * sc_;
+    std::vector<std::string> symbols_;
+    std::string out_buffer, error_buffer;
+  };
+};
+
+#endif
