7cf6b289e4fb4fb11e4d20ddb0cdcf1d02b70698
Mikael Rekola <mikael.rekola@gmail.com>
2023-08-24 13:55:37 +0300

    add 256-color mode to set_truecolor, add cols and rows to get_window_size

--- src/term.h
+++ src/term.h
@@ -31,13 +31,33 @@ static inline void set_basic_style(FILE * fh, int style) {
   }
 }
 
-static inline void set_truecolor(FILE * fh, double r, double g, double b) {
+static inline void set_truecolor(FILE * fh, double r0, double g0, double b0, bool has_rgb) {
   if (isatty(fileno(fh))) {
-    fprintf(fh, "\033[%d;2;%d;%d;%dm",
-	    38,
-	    clamp((int)(r * 255), 0, 255),
-	    clamp((int)(g * 255), 0, 255),
-	    clamp((int)(b * 255), 0, 255));
+    int r = clamp((int)(r0 * 255), 0, 255);
+    int g = clamp((int)(g0 * 255), 0, 255);
+    int b = clamp((int)(b0 * 255), 0, 255);
+    
+    if (has_rgb) {
+      fprintf(fh, "\033[%d;2;%d;%d;%dm", 38, r, g, b);
+    } else {
+      int color;
+      if ((r / 10) == (g / 10) && (g / 10) == (b / 10)) {
+	r /= 10;
+	if (r == 0) {
+	  color = 0;
+	} else if (r >= 25) {
+	  color = 15;
+	} else {
+	  color = 232 + (r - 1);
+	}
+      } else {
+	r /= 43;
+	g /= 43;
+	b /= 43;
+	color = 16 + r * 6 * 6 + g * 6 + b;
+      }
+      fprintf(fh, "\033[38:5:%dm", color);
+    }
   }
 }
 
@@ -57,46 +77,55 @@ static inline struct termios set_raw_mode(int fd) {
   return orig_term;
 }
 
-static inline bool get_window_size(FILE * in, FILE * out, int * width, int * height) {
-  bool r = false;
-  if (isatty(fileno(out))) {
-    struct winsize ws;
-    
-    if (ioctl(1, TIOCGWINSZ, &ws) != -1 && ws.ws_xpixel && ws.ws_ypixel) {
-      *width = ws.ws_xpixel;
-      *height = ws.ws_ypixel;
-      return true;
-    } else {
-      char buf[32];
-      unsigned int i = 0;
-
-      struct termios orig_term = set_raw_mode(fileno(in));
-      
-      if (write(fileno(out), "\033[14t", 5) != 5) {
-	return false;
-      }
-
-      while (i < sizeof(buf)-1) {
-        if (read(fileno(in), buf+i, 1) != 1) {
-	  fprintf(stderr, "read ended at %d\n", i);
-	  break;
-	}
-        if (buf[i] == 't') {
-	  break;
-	}
-        i++;
-      }
-      buf[i] = '\0';
-
-      if (buf[0] == 27 && buf[1] == '[' && buf[2] == '4' && buf[3] == ';' &&
-	  sscanf(buf + 4, "%d;%dt", height, width) == 2) {
-	r = true;
-      }
-
-      tcsetattr(STDIN_FILENO, TCSADRAIN, &orig_term);
-    }
+static inline bool get_window_size(FILE * in, FILE * out, int * cols, int * rows, int * width, int * height) {
+  if (!isatty(fileno(out))) {
+    return false;
   }
-  return r;
+  
+  struct winsize ws;
+  
+  if (ioctl(1, TIOCGWINSZ, &ws) != -1) {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    *width = ws.ws_xpixel;
+    *height = ws.ws_ypixel;
+  } else {
+    *cols = 80;
+    *rows = 25;
+    *width = 0;
+    *height = 0;
+  }
+  
+  if (!*width || !*height) {
+    char buf[32];
+    unsigned int i = 0;
+    
+    struct termios orig_term = set_raw_mode(fileno(in));
+    
+    if (write(fileno(out), "\033[14t", 5) != 5) {
+      return false;
+    }
+    
+    while (i < sizeof(buf)-1) {
+      if (read(fileno(in), buf+i, 1) != 1) {
+	break;
+      }
+      if (buf[i] == 't') {
+	break;
+      }
+      i++;
+    }
+    buf[i] = '\0';
+    
+    if (buf[0] == 27 && buf[1] == '[' && buf[2] == '4' && buf[3] == ';' &&
+	sscanf(buf + 4, "%d;%dt", height, width) == 2) {
+
+    }
+    
+    tcsetattr(STDIN_FILENO, TCSADRAIN, &orig_term);
+  }
+
+  return true;
 }
 	   
 static bool get_cursor_position(FILE * in, FILE * out, int * x, int * y) {
