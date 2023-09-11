ae6e8c9e75c5eb4d44c357b6884729f3ddf03e8c
Mikael Rekola <mikael.rekola@gmail.com>
2023-09-12 02:57:46 +0300

    add two argument version of file and update documentation

--- lib/clojure.java.io.clj
+++ lib/clojure.java.io.clj
@@ -2,5 +2,11 @@
 
 (def reader java.io.Reader)
 (def writer java.io.Writer)
-(def file java.io.File)
+
+(def file
+  "Returns a java.io.File"
+  (fn
+    ([arg] (java.io.File arg))
+    ([parent child] (clojure.string/join (System/getProperty "file.separator") [ parent child ]))))
+
 (def as-file java.io.File)
