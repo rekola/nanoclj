ff39df806cadb6e31a5c50d78fb3d8a3205b8d1a
Mikael Rekola <mikael.rekola@gmail.com>
2023-10-12 15:37:29 +0300

    add clojure.java.shell.clj

---
+++ lib/clojure.java.shell.clj
@@ -0,0 +1,13 @@
+(in-ns 'clojure.java.shell)
+
+(defn sh
+  "Launch a process and capture output"
+  [arg & args] (let [rdr (clojure.java.io/reader (str "|" (clojure.string/join \space (cons arg args))))
+                     f (fn [s] (let [c (.read rdr)] (if (= c ##Eof) s (recur (conj s c)))))
+                     out (f "")
+                     rv (.close rdr)
+                     ]
+                 {:out out
+                  :exit rv
+                  :err nil}))
+                      
