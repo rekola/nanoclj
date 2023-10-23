181bbfced36406e2b458b0e8a3dc32c63b6d08c6
Mikael Rekola <mikael.rekola@gmail.com>
2023-10-23 03:20:22 +0300

    add fullwidth letters

--- src/nanoclj_utils.h
+++ src/nanoclj_utils.h
@@ -26,6 +26,10 @@ static inline int digit(int32_t c, int radix) {
     c -= 'a' - 10;
   } else if (c >= 'A' && c <= 'Z') {
     c -= 'A' - 10;
+  } else if (c >= 0xff21 && c <= 0xff3a) { /* fullwidth uppercase Latin letters */
+    c -= 0xff21 - 10;
+  } else if (c >= 0xff41 && c <= 0xff5a) { /* fullwidth lowercase Latin letters */
+    c -= 0xff41 - 10;
   } else {
     return -1;
   }
