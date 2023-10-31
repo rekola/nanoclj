b931f37a3e0b254020682c122c2ddeebf97f060b
Mikael Rekola <mikael.rekola@gmail.com>
2023-10-31 08:37:34 +0200

    add clojure.instant.clj and java.lang.Double

---
+++ lib/clojure.instant.clj
@@ -0,0 +1,5 @@
+(in-ns 'clojure.instant)
+
+(defn read-instant-date
+  "Parses a date"
+  [cs] (java.util.Date cs))
---
+++ lib/java.lang.Double.clj
@@ -0,0 +1,23 @@
+(in-ns 'java.lang.Double)
+
+(def BYTES 8)
+(def NaN ##NaN)
+(def NEGATIVE_INFINITY ##-Inf)
+(def POSITIVE_INFINITY ##Inf)
+(def SIZE 64)
+
+(def parseDouble
+  "Parses a string to get a Double"
+  (fn [str] (double str)))
+
+(def isInfinite
+  "Returns true if argument is infinite"
+  (fn [d] (or (== d ##Inf) (== d ##-Inf))))
+
+(def isNaN
+  "Returns true if argument is a NaN"
+  (fn [d] (== d ##NaN)))
+
+
+
+
