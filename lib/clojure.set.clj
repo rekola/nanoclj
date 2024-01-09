52340cc9d61fbc3df77d25aba337db0f6431cdb4
Mikael Rekola <mikael.rekola@gmail.com>
2024-01-10 01:21:10 +0200

    add clojure.set/difference, clojure.set/superset?, clojure.set/select

--- lib/clojure.set.clj
+++ lib/clojure.set.clj
@@ -11,9 +11,33 @@
   "Returns the intersection of the given sets"
   ([A] A)
   ([A B] (loop [r #{ } A (seq A)] (if A
-                                    (if (get B (first A))
+                                    (if (contains? B (first A))
                                       (recur (conj r (first A)) (next A))
                                       (recur r (next A)))
                                     r)))
   ([A B & more] (reduce intersection (intersection A B) more)))
 
+(defn difference
+  "Returns a set with elements in A that do not belong to the other sets"
+  ([A] A)
+  ([A B] (loop [r #{ } A (seq A)] (if A
+                                    (if (contains? B (first A))
+                                      (recur r (next A))
+                                      (recur (conj r (first A)) (next A)))
+                                    r)))
+  ([A B & more] (reduce difference (difference A B) more)))
+
+(defn superset?
+  "Returns true if A is a superset of B"
+  ([A B] (loop [B (seq B)] (if B
+                             (if (contains? A (first B))
+                               (recur (next B))
+                               false)
+                             true))))
+
+(defn select
+  [pred A] (loop [r #{ } A (seq A)] (if A
+                                      (if (pred (first A))
+                                        (recur (conj r (first A)) (next A))
+                                        (recur r (next A)))
+                                      r)))
--- tests/set.clj
+++ tests/set.clj
@@ -6,3 +6,10 @@
 (t/is (= (s/intersection #{ 1 2 3 4 5 } #{ 4 5 6 7 }) #{ 4 5 }))
 (t/is (= (s/intersection #{1 :a} #{:a 3} #{:a}) #{ :a }))
 (t/is (= (s/intersection #{:a :b :c } #{:b :c :e} #{:f}) #{}))
+
+(t/is (= (s/difference #{ "One" "Two" "Three" "Four" } #{ 1 2 "Three" } #{ "Four"}) #{ "Two" "One" }))
+
+(t/is (s/superset? #{ :a :b :c :d } #{ :a }))
+(t/is (not (s/superset? #{ :a :b :c :d } #{ 1 2 })))
+
+(t/is (= (s/select odd? (into #{} (range 10))) #{ 1 3 5 7 9 }))
